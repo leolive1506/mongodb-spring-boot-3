bf784c0037df8b81e34b53ea39de3fb972bdbc68
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-21 10:49:16 -0300

    search post by title with @Query

--- src/main/java/com/santam/workshopmongo/repositorie/PostRepository.java
+++ src/main/java/com/santam/workshopmongo/repositorie/PostRepository.java
@@ -6,8 +6,13 @@ import com.santam.workshopmongo.domain.Post;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
   List<Post> findByTitleContainingIgnoreCase(String text);  
+
+  // ?0 = primeiro parametro da função
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+  List<Post> searchTitle(String text);
 }
